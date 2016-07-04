685460be4114080632856daf6fbc68bc9304f587
Tim Sloan <timsloan@gmail.com>
2016-07-04 10:56:06 -0600

    Update __init__.py
    fixing things up for Python 3

--- news_sitemaps/__init__.py
+++ news_sitemaps/__init__.py
@@ -1,8 +1,9 @@
 from django.conf import settings
-from sitemaps import NewsSitemap
+from .sitemaps import NewsSitemap
 
 registry = {}
 
+
 def register(**kwargs):
-    for name,sitemap in kwargs.items():
+    for name, sitemap in kwargs.items():
         registry[name] = sitemap
